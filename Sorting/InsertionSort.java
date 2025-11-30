e07208790cda37e5165955eb80a944425a585570
Ayush Karma <ayushkarma.dev@gmail.com>
2025-11-30 15:26:21 +0530

    done with basic sorting

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,30 @@
+package Sorting;
+
+public class InsertionSort {
+    //- Time complexity = O(n^2)
+     public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {7,8,3,1,2};
+        int count = 0;
+
+        for(int i = 1; i<arr.length; i++){
+            int current = arr[i];
+            int j = i-1;
+            while( j >= 0 && current < arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            // placement
+            arr[j+1] = current;
+            count++;
+        }
+
+        printArray(arr);
+        System.out.println(); 
+        System.out.println("Total swaps taken: " + count);
+    }
+}
---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,31 @@
+package Sorting;
+
+public class SelectionSort {
+    //- Time complexity = O(n^2)
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {7,8,3,1,2};
+        int count = 0;
+
+        for(int i = 0; i<arr.length-1; i++){
+            int smallest = i;
+            for(int j = i+1; j<arr.length; j++){
+                if(arr[smallest] > arr[j]){
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+            count++;
+        }
+        printArray(arr);
+        System.out.println(); 
+        System.out.println("Total swaps taken: " + count);
+    }
+
+}

66f5d3502b4da4e54d4987387d2f6301c6742191
Ayush Karma <ayushkarma.dev@gmail.com>
2025-12-01 18:07:26 +0530

    Done with Recursion

---
+++ Recursion/RecursionBasics.java
@@ -0,0 +1,27 @@
+package Recursion;
+
+public class RecursionBasics {
+    // public static void Print(int n){
+    //    if(n == 0) return;
+    //    System.out.println(n);
+    //    Print(n-1);
+    // }
+    // public static void main(String[] args) {
+    //     int n = 5;
+    //     Print(n);
+    // }
+
+    //> Sum of n natural numbers.
+    // public static void printSum(int i, int n, int sum) {
+    //     if(i == n){
+    //         sum += i;
+    //         System.out.println(sum);
+    //         return;
+    //     }
+    //     sum += i;
+    //     printSum(i+1, n, sum);
+    // }
+    // public static void main(String[] args) {
+    //     printSum(1, 5, 0);
+    // }
+}

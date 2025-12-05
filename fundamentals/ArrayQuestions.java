7c38f82dd4b073e62b43f4b5b24c66331c3eac3a
Ayush Karma <ayushkarma.dev@gmail.com>
2025-12-05 15:18:56 +0530

    Day 6

--- fundamentals/ArrayQuestions.java
+++ fundamentals/ArrayQuestions.java
@@ -1,8 +1,17 @@
 package Fundamentals;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArrayQuestions {
+
+    //> Helper function
+
+    static void printArr(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] +  " ");
+        }
+    }
     //> Q 1 Find the total number of pairs in the Array whose sun is equal to the give vlaue x
     public static void findSum(int arr[], int x) {
         for(int i = 0; i < arr.length-1; i++){
@@ -50,6 +59,97 @@ public class ArrayQuestions {
         return ans;
     }
 
+    //> Q 4 Find second max number
+    public static int findMax(int[] arr) {
+        int maxNumber = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > maxNumber){
+                maxNumber = arr[i];
+            }
+        }
+        return maxNumber ;
+    }
+    static int findSecondMax(int[] arr) {
+        int maxNum = findMax(arr);
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == maxNum){
+                arr[i] = Integer.MIN_VALUE;
+            }
+        }
+        int secondMax = findMax(arr);
+        return secondMax;
+    }
+
+    //> Q 5 Find the first repeating value 
+
+    static int findRepeating(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[i] == arr[j]){
+                    return arr[i];
+                }
+            }
+        }
+        return -1;
+    }
+
+    //> Q 6 Reverse an array
+    static int[] reverseArr(int[] arr){
+        int n = arr.length;
+        int ans[] = new int[n];
+        int j = 0;
+
+        for(int i = n-1; i >= 0; i--){
+            ans[j++] = arr[i];
+        }
+        return ans;
+    }
+
+    //> Q 7 Reverse an array inplace
+    static int[] reverseArrInPlace(int arr[]){
+        int n = arr.length;
+        int i = 0, j = n - 1;
+
+        while(i < j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i++;
+            j--;
+        }
+        return arr;
+    }
+
+    //> Q 8 Rotate and array of length n till k times
+
+    static int[] rotateArr(int arr[], int k){
+        int n = arr.length;
+        k = k % n;
+        int[] ans = new int[n];
+        int j = 0;
+
+        for(int i = n-k; i < n; i++){
+            ans[j++] = arr[i];
+        }
+        for(int i = 0; i < n-k; i++){
+            ans[j++] = arr[i];
+        }
+
+        return ans;
+    }
+
+    //> Q 9 
+
+    static int[] freqArr(int arr[]){
+        int fqArr[] = new int[1000005];
+        for(int i = 0; i < arr.length; i++){
+            fqArr[arr[i]]++;
+        }
+        return fqArr;
+    }
+
     public static void main(String[] args) {
         //> Q1 
         // int arr_Q1[] = {4,6,3,5,8,2};
@@ -62,7 +162,50 @@ public class ArrayQuestions {
         // countTriplet(arr_Q2, target2);  
         
         //> Q3
-        int arr_Q3[] = {1,2,2,1,3};
-        System.out.println(findUniqueElement(arr_Q3));
+        // int arr_Q3[] = {1,2,2,1,3};
+        // System.out.println(findUniqueElement(arr_Q3));
+
+        //> Q4
+        // int arr_Q4[] = {5,8,12,11,6,1};
+        // System.out.println("Second maximun number is: " + findSecondMax(arr_Q4));
+
+        //> Q5
+        // int arr_Q5[] = {5,8,11,11,6,5};
+        // System.out.println(findRepeating(arr_Q5));
+
+        //> Q6
+        // int arr_Q6[] = {1,2,3,4,5,6};
+        // int ans[] = reverseArr(arr_Q6);
+        // printArr(ans);
+
+        //> Q7
+        // int arr_Q7[] = {1,2,3,4,5};
+        // int ans[] = reverseArrInPlace(arr_Q7);
+        // printArr(ans);
+
+        //> Q8
+        // int arr_Q8[] = {1,2,3,4,5};
+        // int k = 2;
+        // int ans[] = rotateArr(arr_Q8, k);
+        // printArr(ans);
+
+        //> Q9
+
+        int arr_Q9[] = {10, 51, 2, 78, 66, 3, 4};
+        int[] freq = freqArr(arr_Q9);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number to querys: ");
+        int q = sc.nextInt();
+
+        while(q > 0){
+            System.out.print("Enter number to searched: ");
+            int x = sc.nextInt();
+            if(freq[x] > 0){
+                System.out.println("Yes");
+            }else{
+                System.out.println("No");
+            }
+            q--;
+        }
     }
 }

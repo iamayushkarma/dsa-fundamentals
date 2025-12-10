ee50a95fb9ab702936097877b11debab0b809a9d
Ayush Karma <ayushkarma.dev@gmail.com>
2025-12-10 12:38:50 +0530

    Complete basic recursion concepts

--- Recursion/RecursionBasics.java
+++ Recursion/RecursionBasics.java
@@ -1,27 +1,29 @@
 package Recursion;
 
 public class RecursionBasics {
-    // public static void Print(int n){
-    //    if(n == 0) return;
-    //    System.out.println(n);
-    //    Print(n-1);
-    // }
-    // public static void main(String[] args) {
-    //     int n = 5;
-    //     Print(n);
-    // }
+    static void printNumbers(int a){
+        if(a == 1){
+            System.out.println(a);
+            return;
+        }
+        printNumbers(a-1);
+        System.out.println(a);
+    }
+    
+    static void printDecresing(int b){
+        if(b == 1){
+            System.out.println(b);
+            return;
+        }
+        System.out.println(b);
+        printDecresing(b-1);
+    }
 
-    //> Sum of n natural numbers.
-    // public static void printSum(int i, int n, int sum) {
-    //     if(i == n){
-    //         sum += i;
-    //         System.out.println(sum);
-    //         return;
-    //     }
-    //     sum += i;
-    //     printSum(i+1, n, sum);
-    // }
-    // public static void main(String[] args) {
-    //     printSum(1, 5, 0);
-    // }
+    public static void main(String[] args) {
+        int a = 5;
+        printNumbers(a);
+        System.out.println();
+        int b = 5;
+        printDecresing(b);
+    }
 }

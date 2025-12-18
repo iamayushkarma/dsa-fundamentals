4438b7c3e2800ae9ff56ca28c81e1d07b0945847
Ayush Karma <ayushkarma.dev@gmail.com>
2025-12-18 14:19:43 +0530

    Add sorting problem

--- Sorting/SortingProblem.java
+++ Sorting/SortingProblem.java
@@ -58,6 +58,76 @@ public class SortingProblem {
 
     }
 
+    // > Q3
+    // Given an array where all its elements are sorted in increasing order except
+    // two swapped elements, sort it in linear time. Assume there are no duplicates
+    // in the array.
+
+    static void swortArr(int[] arr) {
+        int n = arr.length;
+        int x = -1, y = -1;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i - 1] > arr[i]) {
+                if (x == -1) { // first conflict
+                    x = i - 1;
+                    y = i;
+                } else {// second conflict
+                    y = i;
+                }
+            }
+        }
+
+        swap(arr, x, y);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    // > Q4
+    // Given an array of positive and negative integers, segregate them in linear
+    // time and constant space. The output should print all negative numbers,
+    // followed by all positive numbers.
+    static void partition(int[] arr) {
+        int l = 0, r = arr.length - 1;
+        while (l < r) {
+            while (arr[l] < 0)
+                l++;
+            while (arr[r] >= 0)
+                r--;
+            if (l < r) {
+                int temp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = temp;
+                l++;
+                r--;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    // > Q5
+    // Given an array of size N containing only Os, Is, and 2s; sort the array in
+    // ascending order.
+
+    static void sort012(int[] a) {
+        int lo = 0, mid = 0, hi = a.length - 1;
+        // explore the unknown region
+        while (mid <= hi) {
+            if (a[mid] == 0) {
+                swap(a, mid, lo);
+                mid++;
+                lo++;
+            } else if (a[mid] == 1) {
+                mid++;
+            } else {
+                swap(a, mid, hi);
+                hi--;
+            }
+        }
+        System.out.println(Arrays.toString(a));
+    }
+
     public static void main(String[] args) {
 
         // > Q1
@@ -67,5 +137,18 @@ public class SortingProblem {
         // > Q2
         String[] arr2 = { "papaya", "lime", "watermelon", "apple", "mango", "kiwi" };
         sortInLexicographi(arr2);
+
+        // > Q3
+        int[] arr3 = { 10, 5, 6, 7, 8, 9, 3 };
+        swortArr(arr3);
+
+        // > Q4
+        int[] arr4 = { -13, 20, 7, 0, -13, 11, -13 };
+        partition(arr4);
+
+        // > 5
+        int[] arr5 = { 2, 2, 0, 0, 1, 1, 2, 0, 1, 0 };
+        sort012(arr5);
+
     }
 }

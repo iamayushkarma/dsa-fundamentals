7233693e88b9a24d80fd7d612829b3746b017461
Ayush Karma <ayushkarma.dev@gmail.com>
2025-12-29 18:58:16 +0530

    Add Doubly Linked List

---
+++ linkedlist/DoublyLinkedList.java
@@ -0,0 +1,50 @@
+package linkedlist;
+
+public class DoublyLinkedList {
+    public static class Node {
+        int value;
+        Node prev;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    static Node printDoublyLinkedList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " ");
+            temp = temp.next;
+        }
+
+        return temp;
+    }
+
+    public static void main(String[] args) {
+
+        Node a = new Node(6);
+        Node b = new Node(16);
+        Node c = new Node(60);
+        Node d = new Node(5);
+        Node e = new Node(12);
+        Node f = new Node(8);
+
+        a.prev = null;
+        a.next = b;
+        b.prev = a;
+        b.next = c;
+        c.prev = b;
+        c.next = d;
+        d.prev = c;
+        d.next = e;
+        e.prev = d;
+        e.next = f;
+        f.prev = e;
+        f.next = null;
+
+        // Print Doubly Linked List
+        System.out.println();
+        printDoublyLinkedList(a);
+    }
+}

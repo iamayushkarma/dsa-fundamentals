f66f903f6d4d60eed4e26d1b748de079ca4e0691
Ayush Karma <ayushkarma.dev@gmail.com>
2026-02-28 13:15:36 +0530

    restore local repo after git corruption

---
+++ Fundamentals/Strings.java
@@ -0,0 +1,53 @@
+package Fundamentals;
+
+import java.util.*;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        //> String Declaration
+        // String name = "Messi";
+        // String fullName = "Messi 10";
+        // Scanner sc = new Scanner(System.in);
+        // String n ame = sc.nextLine();
+        // System.out.println("Your name is: " + name);
+
+        //> Concatenation
+        // String firstName = "Tony";
+        // String lastName = "Stark";
+        // String fullName = firstName + " " + lastName;
+        // System.out.println(fullName);
+        // System.out.println(fullName.length());
+
+        //> charAt
+        // for(int i = 0; i < fullName.length(); i++){
+        //     System.out.println(fullName.charAt(i));
+        // }
+
+        //> Compare
+        // String firstName = "Tony";
+        // String lastName = "Tony";
+
+        // s1 > s2: +ve value
+        // s1 == s2: 0
+        // s1 < s2: -ve value
+
+        // if(firstName.compareTo(lastName) == 0){
+        //     System.out.println("Strings are equal");
+        // }else{
+        //     System.out.println("Strings are not equal");
+        // }
+
+        int a = 10;
+        int b = 0;
+        b = ++a;
+        System.out.println(a);
+        System.out.println(b);
+
+        int c = 10;
+        int d = 0;
+        d = c++;
+        System.out.println(c);
+        System.out.println(d);
+    } // end of main function
+}

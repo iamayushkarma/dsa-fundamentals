34d6cca8783c21c95fcea0e8803975af4fd61b49
Ayush Karma <ayushkarma.dev@gmail.com>
2026-03-24 21:49:24 +0530

    Add Q3 to sliding window question

--- SlidingWindow/SlidingWindowQuestions.java
+++ SlidingWindow/SlidingWindowQuestions.java
@@ -34,7 +34,6 @@ public class SlidingWindowQuestions {
     // > Q2 Sort the array and check every window of size m; return the minimum
     // > difference between the first and last element of the window (arr[i+m-1] -
     // > arr[i]).
-
     public static int findMinDiff(int[] arr, int k) {
         int dif = 0;
         int maxDif = Integer.MAX_VALUE;
@@ -48,6 +47,52 @@ public class SlidingWindowQuestions {
         return maxDif;
     }
 
+    // > Q3 Given a number x and an array of integers arr, find the smallest
+    // > subarray with sum greater than the given value. If such a subarray do not
+    // > exist return 0 in that case.
+    public static int smallestSubWithSum(int[] arr, int target) {
+        int ans = Integer.MAX_VALUE;
+        int windowStarting = 0;
+        int n = arr.length;
+        int sum = 0;
+        int length = 0;
+        int i = 0;
+
+        while (i < n) {
+            sum += arr[i];
+            if (sum > target) {
+                length = i - windowStarting + 1;
+                ans = Math.min(ans, length);
+                while (windowStarting < i && sum > target) {
+                    sum -= arr[windowStarting++];
+                    if (sum < target) {
+                        length = i - windowStarting + 1;
+                        ans = Math.min(ans, length);
+                    }
+                }
+            }
+            i++;
+        }
+
+        // for (int i = 0; i < n; i++) {
+        // if (sum <= target) {
+        // sum += arr[i];
+        // System.out.println("Sum " + sum);
+        // if (sum > target) {
+        // length = i - windowStarting + 1;
+        // System.out.println("length " + length);
+        // }
+        // } else if (sum > target) {
+        // sum -= arr[windowStarting];
+        // windowStarting++;
+        // }
+        // System.out.println("Ans " + ans);
+        // ans = Math.min(ans, length);
+        // }
+        return ans;
+
+    }
+
     public static void main(String[] args) {
         // > Q1 First negative in every window of size k
         int[] arr1 = { -8, 2, 3, -6, 10 };
@@ -57,9 +102,15 @@ public class SlidingWindowQuestions {
         // > Q2 Sort the array and check every window of size m; return the minimum
         // > difference between the first and last element of the window (arr[i+m-1] -
         // > arr[i]).
-
         int[] arr2 = { 3, 4, 1, 9, 56, 7, 9, 12 };
         int m = 5;
         System.out.println("Q3 | Minimum difference is: " + findMinDiff(arr2, m));
+
+        // > Q3 Given a number x and an array of integers arr, find the smallest
+        // > subarray with sum greater than the given value. If such a subarray do not
+        // > exist return 0 in that case.
+        int[] arr3 = { 1, 4, 45, 6, 0, 19 };
+        int value3 = 51;
+        System.out.println("Q3 | Sub array sum is : " + smallestSubWithSum(arr3, value3));
     }
 }
